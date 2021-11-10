c902c308051cbb8eb6385ee12963935e787c00e3
Han <55072934+aryosatria@users.noreply.github.com>
2021-11-10 08:45:55 +0700

    Add files via upload

---
+++ dht_ubidots/dht_ubidots.ino
@@ -0,0 +1,37 @@
+#include <DHT.h>
+#include <UbidotsMicroESP8266.h> // import file ubidots
+
+#define DHTPIN 4 // mendefiniskan pin dht di nodemcu adalah 4, D2 = 4
+#define DHTTYPE DHT11 // mendefinisikan tipe dht yang kita gunakan
+
+char auth[] = "BBFF-xoVuy3w4QNUQEzH7AoLiID3HtPG6xP";
+char ssid[] = "IDN Boarding School";
+char pass[] = "";
+
+DHT dht(DHTPIN, DHTTYPE);
+Ubidots client(auth); // setting token client di ubidots
+unsigned long last = 0;
+ 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); 
+  dht.begin(); // dht mulaimembaca suhu dan kelembapan
+  delay(20); // delay 0,02 detik
+  client.wifiConnection(ssid,pass); // menyambungkan ubidots client dengan wifi
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(millis()-last>10000){ // jika sudah 10 detik
+     float hum = dht.readHumidity();
+     float temp = dht.readTemperature();
+
+     last = millis(); // 11
+     client.add("kelembapan",hum); // ubidots client membaca kelembapan
+     client.add("Temperatur",temp); // ubidots client membaca suhu
+     client.sendAll(true); // ubidots client mengirimkan semua datan
+     
+  }
+
+}
